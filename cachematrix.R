acd02ed1e4b8a02bc4750b3634b9d23319a678da
knishantxlri <knishantxlri@gmail.com>
2014-12-15 15:17:20 +0530

    Revert "Initial commit for Programming Assignment 2"
    This reverts commit 27987823fcf81d46a5e2186391addd87c079b879.
    
    Conflicts:
    	README.md
    	cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a special "matrix" object that can cache its inverse
+## set the value of the matrix, get the value of the matrix
+#set the value of the inverse, get the value of the inverse
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(c(1,2,3,4),nrow =2, ncol = 2)){
+      inv <- NULL
+      
+#method to assign new value to the object
+      set <- function(y){
+          x <<- y
+          inv <<- NULL
+      }
+      
+#method to get the value of object
+      get <- function(){x}
+      
+#method to set matrix inverse
+      setinverse <- function (inverse){
+          inv <<- inverse
+      }
+      
+#method to get the inverse
+      getinverse <- function(){inv}
+      
+      list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
+#If the inverse has already been calculated (and the matrix has not changed), 
+#then cacheSolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x,...){
+#get the value of inverse
+      inv <- x$getinverse()
+#check if inverse is already there, then return inverse
+      if(!is.null(inv)){
+          message("getting the cached data")
+          return(inv)
+      }
+#get matrix for calculating inverse
+      mat <- x$get()
+#calculate inverse of the matrix
+      inv <- solve(mat)
+#set the value of inverse
+      x$setinverse(inv)
+#return inverse
+      inv 
+}  
